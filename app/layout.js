ec7692274c688f3b1a2968f420ba0c4c2a4155df
ibtissammassa <bettysama16@gmail.com>
2024-04-29 23:16:25 +0100

    Home icon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Cairo } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import Home from "../public/home.svg";
+import Link from "next/link";
 
 const cairo = Cairo({ subsets: ["latin"] });
 
@@ -11,7 +14,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={cairo.className}>{children}</body>
+      <body className={"relative " + cairo.className}>
+        <Link href={"/"}>
+          <Image
+            className="absolute top-9 left-9 rounded-full hover:bg-blue-600 cursor-pointer p-2 shadow-lg"
+            width={50}
+            src={Home}
+            alt="home"
+          />
+        </Link>
+        {children}
+      </body>
     </html>
   );
 }
