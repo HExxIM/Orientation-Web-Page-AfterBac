5c90721a78940a4c5409723d557643f76c1cab57
hassanelq <hassanelqadi2@gmail.com>
2024-04-29 22:25:56 +0100

    Update page.jsx to dynamically generate buttons based on Schools in Fields.js

--- app/[Field]/page.jsx
+++ app/[Field]/page.jsx
@@ -1,17 +1,14 @@
 import Button from "../Components/Button";
+import Fields from "../Database/Fields";
 
 export default function OpenLimitedPage({ params }) {
+  const FieldIndex = Fields.findIndex((field) => field.slug === params.Field);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Button
-          title="كليات ذات الإستقطاب المفتوح"
-          link={`/${params.Field}//Open`}
-        />
-        <Button
-          title="كليات ذات الاستقطاب المحدود"
-          link={`/${params.Field}/Limited`}
-        />
+        {Fields[FieldIndex].Schools.map((School, index) => (
+          <Button key={index} title={School.Name} link={`/`} />
+        ))}
       </div>
     </main>
   );
