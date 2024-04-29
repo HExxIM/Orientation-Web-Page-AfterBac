90188cb0516845611e8dd6e47c3be1ddb8637d8c
hassanelq <hassanelqadi2@gmail.com>
2024-04-30 00:36:23 +0100

    Refactor page.jsx to fix import statement and improve code formatting

--- app/[Field]/page.jsx
+++ app/[Field]/page.jsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 import Button from "../Components/Button";
 import Fields from "../Database/Fields";
 import Image from "next/image";
 import Home from "../../public/home.svg";
 import Link from "next/link";
-import SchoolPopup from "../Components/Popup"
+import SchoolPopup from "../Components/Popup";
 import { useState } from "react";
 
 export default function OpenLimitedPage({ params }) {
@@ -22,18 +22,20 @@ export default function OpenLimitedPage({ params }) {
         />
       </Link>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="mt-[50px] relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] text-5xl">
+        <h1 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] text-5xl text-center">
           {Fields[FieldIndex].field}
         </h1>
       </div>
       <div className="mb-32 flex flex-wrap gap-6 text-center justify-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-right">
         {Fields[FieldIndex].Schools.map((School, index) => (
-          <Button setShowSchoolPopup={setShowSchoolPopup} key={index} title={School.Name} />
+          <Button
+            setShowSchoolPopup={setShowSchoolPopup}
+            key={index}
+            title={School.Name}
+          />
         ))}
       </div>
-      {
-        showSchoolPopup && <showSchoolPopup />
-      }
+      {showSchoolPopup && <showSchoolPopup />}
     </main>
   );
 }
