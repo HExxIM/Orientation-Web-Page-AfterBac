8432f6b048cd8ff493ad143761ffc297b6f39d7c
ibtissammassa <bettysama16@gmail.com>
2024-04-30 00:08:20 +0100

    Refactor Button component to use conditional rendering for the link and improve code readability

--- app/Components/Button.js
+++ app/Components/Button.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
 export default function Button({ title, link }) {
+  const Comp = link ? Link : "div";
   return (
-    <Link
+    <Comp
       href={link}
-      className="relative inline-flex lg:w-auto w-full items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
+      className="relative cursor-pointer inline-flex lg:w-auto w-full items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
     >
       <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
       <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
@@ -12,6 +13,6 @@ export default function Button({ title, link }) {
         {title}
       </span>
       <span className="absolute inset-0 border-2 border-white rounded-full"></span>
-    </Link>
+    </Comp>
   );
 }
