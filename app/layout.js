3475949feebdf760bb08c651a09c435b977fbf8f
ibtissammassa <bettysama16@gmail.com>
2024-04-29 23:23:41 +0100

    turn home to the right

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
       <body className={"relative " + cairo.className}>
         <Link href={"/"}>
           <Image
-            className="absolute top-9 left-9 rounded-full hover:bg-blue-600 cursor-pointer p-2 shadow-lg"
+            className="absolute top-9 right-9 rounded-full hover:bg-blue-600 cursor-pointer p-2 shadow-lg"
             width={50}
             src={Home}
             alt="home"
