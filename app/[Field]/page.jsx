fd33688544fc7531d3e83c676a80fdb01eee72ea
hassanelq <hassanelqadi2@gmail.com>
2024-04-30 00:07:26 +0100

    Refactor styling and layout in page.jsx to improve readability and responsiveness

--- app/[Field]/page.jsx
+++ app/[Field]/page.jsx
@@ -19,11 +19,11 @@ export default function Schools({ params }) {
         />
       </Link>
       <div className="relative flex place-items-center before:absolute before:h-[400px] before:w-full sm:before:w-[600px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-3xl before:content-[''] after:absolute after:-z-20 after:h-[250px] after:w-full sm:after:w-[300px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-300 after:via-blue-300 after:blur-3xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-800 before:dark:opacity-20 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[500px] z-[-1]">
-        <h1 className=" relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] text-5xl">
+        <h1 className=" relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] text-center text-5xl">
           {Fields[FieldIndex].field}
         </h1>
       </div>
-      <div className="mb-32 flex flex-wrap gap-6 text-center justify-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-right">
+      <div className="mb-5 flex flex-wrap gap-6 text-center justify-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-right">
         {Fields[FieldIndex].Schools.map((School) => (
           <SchoolPopup SchoolData={School} />
         ))}
