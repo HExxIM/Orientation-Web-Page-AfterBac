16d6a7575d30d853d3aece83eddf71015cf70d01
hassanelq <hassanelqadi2@gmail.com>
2024-04-30 18:22:35 +0100

    Update school description and address display in Popup.js and page.jsx

--- app/Components/Popup.js
+++ app/Components/Popup.js
@@ -18,13 +18,14 @@ export default function SchoolPopup({ setShowSchoolPopup, school }) {
             }}
           />
           <h1 className="text-3xl font-bold">{school.Name}</h1>
-          <p className="text-lg ">
-            <br></br>
-            {school.Description}
-          </p>
           {school.Address && (
             <h2 className="text-2xl mt-4">{school.Address}</h2>
           )}
+          <p className="text-lg ">
+            <br></br>
+            {school.Description}
+            <br></br>
+          </p>
         </div>
       </div>
     </div>
