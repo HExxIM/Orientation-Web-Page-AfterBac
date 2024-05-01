fae79d9caad5ddd75d4d32a3be929ee83ad847fa
ibtissammassa <bettysama16@gmail.com>
2024-05-01 10:50:46 +0100

    Fix responsive problems

--- app/Components/Button.js
+++ app/Components/Button.js
@@ -6,7 +6,7 @@ export default function Button({ title, link }) {
   return (
     <Comp
       href={link}
-      className="relative cursor-pointer inline-flex lg:w-auto items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
+      className="relative w-full cursor-pointer inline-flex lg:w-auto items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
     >
       <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
       <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
