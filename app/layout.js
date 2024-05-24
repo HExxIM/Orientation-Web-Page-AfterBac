8b5d0b59f2c9294ba06b69522b2532585d4a146a
hassanelq <hassanelqadi2@gmail.com>
2024-05-24 21:09:54 +0100

    Refactor layout in RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Cairo } from "next/font/google";
 import "./globals.css";
+import ClientRootLayout from "./ClientRootLayout"; // Ensure this path is correct
 
 const cairo = Cairo({ subsets: ["latin"] });
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={"relative " + cairo.className}>{children}</body>
+      <body className={`relative ${cairo.className}`}>
+        <ClientRootLayout>{children}</ClientRootLayout>
+      </body>
     </html>
   );
 }
