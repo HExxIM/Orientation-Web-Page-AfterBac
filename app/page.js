b83febc9c719144f656f140fba2f72b803c4f21e
hassanelq <hassanelqadi2@gmail.com>
2024-05-24 21:36:51 +0100

    Refactor layout and styling in page.jsx

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Fields from "./Database/Fields";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-20">
-      <div className="relative flex place-items-center before:absolute before:h-[400px] before:w-full sm:before:w-[600px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-black before:to-transparent before:blur-3xl before:content-[''] after:absolute after:-z-20 after:h-[250px] after:w-full sm:after:w-[300px] after:translate-x-1/3 after:bg-gradient-conic after:from-black after:via-black after:blur-3xl after:content-[''] before:lg:h-[500px] z-[-1]">
+      <div className="relative flex place-items-center before:absolute before:h-[400px] before:w-full sm:before:w-[600px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-3xl before:content-[''] after:absolute after:-z-20 after:h-[250px] after:w-full sm:after:w-[300px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-300 after:via-blue-300 after:blur-3xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-800 before:dark:opacity-20 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[500px] z-[-1]">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] invert"
           src="/education_logo.png"
