78b41f20c15cb9988d8998fa1547bb40cc5cf890
hassanelq <hassanelqadi2@gmail.com>
2024-05-24 21:23:04 +0100

    Refactor layout and styling in page.jsx

--- app/[Field]/page.jsx
+++ app/[Field]/page.jsx
@@ -13,7 +13,7 @@ export default function OpenLimitedPage({ params }) {
   const [school, setSchool] = useState(null);
 
   return (
-    <main className="flex flex-col gap-[6rem] items-center justify-center p-6 pb-16 h-[100%] overflow-x-hidden">
+    <main className="flex flex-col gap-[6rem] items-center justify-center p-6 pb-16 h-[100%] overflow-x-hidden bg-black text-white">
       <Link href={"/"}>
         <Image
           className="absolute top-9 right-9 rounded-full hover:bg-blue-600 cursor-pointer p-2 shadow-lg"
@@ -23,8 +23,8 @@ export default function OpenLimitedPage({ params }) {
           priority
         />
       </Link>
-      <div className="mt-20 md:mt-52 lg:mt-auto relative flex place-items-center before:absolute before:h-[400px] before:max-w-full before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-3xl before:content-[''] after:absolute after:-z-20 after:h-[250px] after:max-w-full after:bg-gradient-conic after:from-sky-300 after:via-blue-300 after:blur-3xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-800 before:dark:opacity-20 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[500px] z-[-1]">
-        <h1 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] text-5xl text-center">
+      <div className="mt-20 md:mt-52 lg:mt-auto relative flex place-items-center before:absolute before:h-[400px] before:max-w-full before:rounded-full before:bg-gradient-radial before:from-black before:to-transparent before:blur-3xl before:content-[''] after:absolute after:-z-20 after:h-[250px] after:max-w-full after:bg-gradient-conic after:from-black after:via-black after:blur-3xl after:content-[''] before:lg:h-[500px] z-[-1]">
+        <h1 className="relative drop-shadow-[0_0_0.3rem_#ffffff70] text-5xl text-center">
           {Fields[FieldIndex] && Fields[FieldIndex].field}
         </h1>
       </div>
